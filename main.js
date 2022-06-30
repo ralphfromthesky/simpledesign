b7cf974922a1c2a3f67cca46c631152cd0542b89
ralph <smmvaralph@yahoo.com>
2022-06-30 22:37:41 +0800

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+var hamburger = document.getElementById('hamburger');
+var navlist = document.getElementById('navlist');
+
+hamburger.addEventListener('click', ()=> {
+    navlist.classList.toggle('show');
+    console.log('buttion clicked');
+});
+
+
+
+    let a = 2 + 2;
+    console.log(a);
